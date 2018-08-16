341a87b1f73a2eb0cc882b224c2eb9285427c902
Lucas Frey <lucas.frey@heyday.co.nz>
2018-08-16 21:23:55 +1200

    Init to win it

---
+++ src/components/layout.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Helmet from 'react-helmet'
+import { StaticQuery, graphql } from 'gatsby'
+
+import Header from './Header';
+import Footer from './Footer';
+import About from './About';
+import Resume from './Resume';
+import Portfolio from './Portfolio';
+
+import './layout.css'
+import '../css/magnific-popup.css';
+import '../css/default.css';
+import '../css/layout.css';
+import '../css/media-queries.css';
+
+const Layout = ({ children, data }) => (
+  <StaticQuery
+    query={graphql`
+      query SiteTitleQuery {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }
+    `}
+    render={data => (
+      <>
+        <Helmet
+          title={data.site.siteMetadata.title}
+          meta={[
+            { name: 'description', content: 'Sample' },
+            { name: 'keywords', content: 'sample, something' },
+          ]}
+        >
+          <html lang="en" />
+        </Helmet>
+        <Header />
+        <About />
+        <Resume />
+        <Portfolio />
+        <Footer />
+        <div
+          style={{
+            margin: '0 auto',
+            maxWidth: 960,
+            padding: '0px 1.0875rem 1.45rem',
+            paddingTop: 0,
+          }}
+        >
+          {children}
+        </div>
+      </>
+    )}
+  />
+)
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
