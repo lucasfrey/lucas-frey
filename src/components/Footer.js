f4e5e2fa7c001c51971c78a02b985859982db701
Lucas Frey <lucas.frey@heyday.co.nz>
2018-08-18 18:37:25 +1200

    - fix typo on linkedin link - switch background image - cleanup some files

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,7 +10,7 @@ class Footer extends Component {
         <div className="twelve columns">
            <ul className="social-links">
               <li key="twitter"><a target="_blank"rel="noopener noreferrer" href="https://twitter.com/FreyLucas"><i className="fa fa-twitter"></i></a></li>
-              <li key="linkedin"><a target="_blank" rel="noopener noreferrer" href="www.linkedin.com/in/lucas-frey"><i className="fa fa-linkedin"></i></a></li>
+              <li key="linkedin"><a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/lucas-frey"><i className="fa fa-linkedin"></i></a></li>
               <li key="instagram"><a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/frey.lucas"><i className="fa fa-instagram"></i></a></li>
               <li key="github"><a target="_blank" rel="noopener noreferrer" href="https://github.com/lucasfrey"><i className="fa fa-github"></i></a></li>
            </ul>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 // import { Link } from 'gatsby'
 
-
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import About from '../components/About';
